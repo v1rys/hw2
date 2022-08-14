2f7f3d40494d969bdc45a44f91c7b50f3335c460
v1rys <alaverdi@gmail.com>
2022-08-14 18:53:00 +0700

    add Task10 and Task13

---
+++ Task10/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.WriteLine("Введите Трёхpзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int number1 = number /10;
+int number2 = number1 % 10;
+if ( 99 >number)
+{
+    Console.WriteLine("Вы ввели не трёхзначное число");
+    if (-99 > number)
+    {
+       Console.WriteLine("Вы ввели не трёхзначное число");
+       if ( 1000 <= number)
+       {
+        Console.WriteLine("Вы ввели не трёхзначное число");
+        if (-1000 >=number)
+        {
+          Console.WriteLine("Вы ввели не трёхзначное число");  
+        
+        }
+       }
+
+    }
+}
+else
+{
+  Console.WriteLine("Второе число " + number2);  
+}
---
+++ Task13/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+Console.WriteLine("Введите число");
+int  number = Convert.ToInt32(Console.ReadLine());
+int numder2 = 0;
+if (numder <= 99){
+Console.WriteLine("В этом числе нет третьей цифры ");
+
+while (number>1000)
+{
+    number=number/10;
+
+}
+if (number > 99 ){
+    number2 = number % 10;
+}
+}
+Console.WriteLine($"Третья цифра" + number2);
